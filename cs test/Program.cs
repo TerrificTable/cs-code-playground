d4d0771765cc88743ae923619e43063f10634166
TerrificTable <85793326+TerrificTable@users.noreply.github.com>
2021-08-27 21:15:59 +0200

    Projektdateien hinzufügen.

---
+++ cs test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_Tutorial
+{
+    class Program
+    {
+        static void Main()
+        {
+            string n = Console.ReadLine();
+            SayHello(n);
+            Console.ReadKey();
+
+            void SayHello(string name)
+            {
+                Console.WriteLine("Hello " + name);
+            }
+        }
+    }
+}
